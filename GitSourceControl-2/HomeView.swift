da18f03eea3b5205dc947a4ea9b84a5220750e2b
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-01-06 20:11:59 +0530

    [Feature] Added home view

---
+++ GitSourceControl-2/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  GitSourceControl-2
+//
+//  Created by mac on 06/01/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("1")
+            Text("2")
+            Text("3")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
