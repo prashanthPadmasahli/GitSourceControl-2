9c22077f8c42937174962567307ed68770cd3790
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-01-06 13:15:57 +0530

    Initial Commit

---
+++ GitSourceControl-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitSourceControl-2
+//
+//  Created by mac on 06/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitSourceControl-2/GitSourceControl_2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControl_2App.swift
+//  GitSourceControl-2
+//
+//  Created by mac on 06/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControl_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
