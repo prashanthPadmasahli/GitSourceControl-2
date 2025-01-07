04dcb6d3ad12a7752f7b262555193db3c5022a2f
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-01-07 22:37:03 +0530

    commit

--- GitSourceControl-2/SettingsView.swift
+++ GitSourceControl-2/SettingsView.swift
@@ -11,7 +11,7 @@ struct SettingsView: View {
     var body: some View {
         VStack {
             Text("Settings")
-            Text("Labeel-3")
+            Text("Labeel-o")
         }
     }
 }
