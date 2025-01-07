7e56a5d01ccd070cb068522af43b01f2df44d7ed
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-01-07 20:05:43 +0530

    [Feature]Added settings view

---
+++ GitSourceControl-2/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  GitSourceControl-2
+//
+//  Created by mac on 07/01/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
